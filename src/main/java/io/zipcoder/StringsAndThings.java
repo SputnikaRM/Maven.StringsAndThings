bd8c50312999b763972a227bf0a4500e350dcc0b
sputnika <sputnika91@gmail.com>
2019-05-30 20:01:08 -0400

    update

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -6,16 +6,23 @@ package io.zipcoder;
  */
 public class StringsAndThings {
 
+
+
     /**
      * Given a string, count the number of words ending in 'y' or 'z' -- so the 'y' in "heavy" and the 'z' in "fez" count,
      * but not the 'y' in "yellow" (not case sensitive). We'll say that a y or z is at the end of a word if there is not an alphabetic
      * letter immediately following it. (Note: Character.isLetter(char) tests if a char is an alphabetic letter.)
      * example : countYZ("fez day"); // Should return 2
-     *           countYZ("day fez"); // Should return 2
-     *           countYZ("day fyyyz"); // Should return 2
+     * countYZ("day fez"); // Should return 2
+     * countYZ("day fyyyz"); // Should return 2
      */
-    public Integer countYZ(String input){
-        return null;
+
+
+public Integer countYZ(String input){
+
+   input = "";
+   return 1;
+
     }
 
     /**
@@ -28,7 +35,10 @@ public class StringsAndThings {
      *           removeString("Hello there", "x") // Should return "Hello there"
      */
     public String removeString(String base, String remove){
-        return null;
+
+        base= base.replaceAll(remove, "");
+
+        return base;
     }
 
     /**
